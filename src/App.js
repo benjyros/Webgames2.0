a011b0d6534e6451b875fc1f416dcc558207405b
benjyros <benny.peterhans@gmail.com>
2023-04-23 01:22:38 +0200

    added reactionclicker without design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Speedclicker from './components/Speedclicker';
+import Reactionclicker from './components/Reactionclicker';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/speedclicker' element={<Speedclicker />} />
+        <Route exact path='/reactionclicker' element={<Reactionclicker />} />
       </Routes>
     </div>
   );
