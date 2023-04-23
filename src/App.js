27c22932f40f5a2c852b99b0869f4f9839e96386
Benjamin Peterhans <111268946+benjyros@users.noreply.github.com>
2023-04-23 22:21:28 +0200

    added hangman with wordlist as a json file (#12)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import TicTacToe from './components/TicTacToe';
+import Hangman from './components/Hangman';
 import Speedclicker from './components/Speedclicker';
 import Reactionclicker from './components/Reactionclicker';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/tictactoe' element={<TicTacToe />} />
+        <Route exact path='/hangman' element={<Hangman />} />
         <Route exact path='/speedclicker' element={<Speedclicker />} />
         <Route exact path='/reactionclicker' element={<Reactionclicker />} />
       </Routes>
