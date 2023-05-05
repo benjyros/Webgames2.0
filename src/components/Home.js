b8df4832752f5c0c9d51569327776a243d93d629
benjyros <benny.peterhans@gmail.com>
2023-05-05 13:19:01 +0200

    edited transitionduration, added responsive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,12 +18,12 @@ export default function Home() {
         const src = `${window.location.origin}/Webgames2.0/images/${webpage.webpage.toLocaleLowerCase()}.png`;
 
         return (
-            <Link className='group transition duration-200 ease-in-out flex text-black items-center justify-center rounded bg-[#ffe0e9] hover:bg-[#eb8faf] h-60' to={location} key={index}>
+            <Link className='group transition duration-200 ease-in-out flex text-black items-center justify-center rounded bg-[#ffe0e9] hover:bg-[#eb8faf] h-64 p-10' to={location} key={index}>
                 <div>
-                    <img className='mx-auto w-[75px] h-[75px] transition duration-150 ease-in-out group-hover:scale-105' src={src} alt={webpage.webpage} />
+                    <img className='mx-auto w-[75px] h-[75px] transition duration-200 ease-in-out group-hover:scale-105' src={src} alt={webpage.webpage} />
                     <hr className='m-2 border-0 bg-gray-300 h-px' />
-                    <p className='transition duration-150 ease-in-out group-hover:scale-105'>{webpage.webpage}</p>
-                    <p className='transition duration-150 ease-in-out group-hover:scale-105'>{webpage.description}</p>
+                    <p className='transition duration-500 ease-in-out group-hover:scale-105 text-2xl font-medium m-2'>{webpage.webpage}</p>
+                    <p className='transition duration-500 ease-in-out group-hover:scale-105 text-sm m-2'>{webpage.description}</p>
                 </div>
             </Link>
         );
@@ -37,9 +37,9 @@ export default function Home() {
         return (
             <div className='flex text-black items-center justify-center rounded bg-[#ffe0e9] p-5' key={index}>
                 <div>
-                    <p className='text-4xl font-medium'>{webpage.webpage.toUpperCase()}</p>
+                    <p className='text-xl md:text-2xl xl:text-4xl font-medium'>{webpage.webpage.toUpperCase()}</p>
                     <hr className='m-2 border-0 bg-gray-300 h-px' />
-                    <video className='w-[650px] h-[500px]' controls>
+                    <video className='w-[350px] h-[200px] md:w-[500px] md:h-[350px] xl:w-[650px] xl:h-[500px]' controls>
                         <source src={src1} type="video/mp4" />
                         <source src={src2} type="video/ogg" />
                         <source src={src3} type="video/webm" />
@@ -55,7 +55,7 @@ export default function Home() {
             <Navbar />
             <div className="w-2/3 mx-auto mt-24">
                 <p className='text-4xl text-black font-medium mb-4 text-left'>Twäwis-Games</p>
-                <hr className='m-2 border-0 bg-gray-300 h-px' />
+                <hr className='mt-4 mb-6 border-0 bg-gray-300 h-px' />
                 <div className='grid grid-auto grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
                     {getLinks}
                 </div>
