a8eea9a199eab3797478b05e0ec871715a8d503f
benjyros <benny.peterhans@gmail.com>
2023-05-05 13:05:29 +0200

    added videos and added some transition time

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,32 +18,53 @@ export default function Home() {
         const src = `${window.location.origin}/Webgames2.0/images/${webpage.webpage.toLocaleLowerCase()}.png`;
 
         return (
-            <Link className='group flex text-black items-center justify-center rounded bg-[#ffe0e9] hover:bg-[#eb8faf] h-60' to={location} key={index}>
-                <div className='group-hover:scale-105'>
-                    <img className='mx-auto w-[75px] h-[75px]' src={src} alt={webpage.webpage} />
+            <Link className='group transition duration-200 ease-in-out flex text-black items-center justify-center rounded bg-[#ffe0e9] hover:bg-[#eb8faf] h-60' to={location} key={index}>
+                <div>
+                    <img className='mx-auto w-[75px] h-[75px] transition duration-150 ease-in-out group-hover:scale-105' src={src} alt={webpage.webpage} />
                     <hr className='m-2 border-0 bg-gray-300 h-px' />
-                    <p>{webpage.webpage}</p>
-                    <p>{webpage.description}</p>
+                    <p className='transition duration-150 ease-in-out group-hover:scale-105'>{webpage.webpage}</p>
+                    <p className='transition duration-150 ease-in-out group-hover:scale-105'>{webpage.description}</p>
                 </div>
             </Link>
         );
     });
 
+    const getVideos = webpages.map((webpage, index) => {
+        const src1 = `${window.location.origin}/Webgames2.0/videos/${webpage.webpage.toLocaleLowerCase()}.mp4`;
+        const src2 = `${window.location.origin}/Webgames2.0/videos/${webpage.webpage.toLocaleLowerCase()}.ogg`;
+        const src3 = `${window.location.origin}/Webgames2.0/videos/${webpage.webpage.toLocaleLowerCase()}.webm`;
+
+        return (
+            <div className='flex text-black items-center justify-center rounded bg-[#ffe0e9] p-5' key={index}>
+                <div>
+                    <p className='text-4xl font-medium'>{webpage.webpage.toUpperCase()}</p>
+                    <hr className='m-2 border-0 bg-gray-300 h-px' />
+                    <video className='w-[650px] h-[500px]' controls>
+                        <source src={src1} type="video/mp4" />
+                        <source src={src2} type="video/ogg" />
+                        <source src={src3} type="video/webm" />
+                    </video>
+                </div>
+            </div>
+        );
+    });
+
+
     return (
         <section className="bg-[#fff5f8] select-none">
             <Navbar />
-            <div className="w-2/3 mx-auto h-screen mt-24">
+            <div className="w-2/3 mx-auto mt-24">
                 <p className='text-4xl text-black font-medium mb-4 text-left'>Twäwis-Games</p>
                 <hr className='m-2 border-0 bg-gray-300 h-px' />
                 <div className='grid grid-auto grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
                     {getLinks}
                 </div>
             </div>
-            <div className="w-2/3 mx-auto h-screen">
+            <div className="w-2/3 mx-auto mt-24 mb-36">
                 <p className='text-4xl text-black font-medium mb-4 text-left'>Twäwis-Demos</p>
                 <hr className='m-2 border-0 bg-gray-300 h-px' />
-                <div className='grid grid-auto grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
-                    {getLinks}
+                <div className='grid grid-auto grid-cols-1 gap-4'>
+                    {getVideos}
                 </div>
             </div>
             <Footer />
